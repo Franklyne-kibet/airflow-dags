b61ba7ab24155f892d5a02062f2e514fe8ced6f0
Franklyne-kibet <rootkibet@gmail.com>
2023-02-13 18:53:27 +0300

    ingestion script

--- data-ingestion/convert/convert.py
+++ data-ingestion/convert/convert.py
@@ -2,6 +2,10 @@ import pandas as pd
 import pyarrow.csv as pv
 import pyarrow.parquet as pq
 
-df = pv.read_csv("green_tripdata_2019-07.csv.gz", convert_options=pv.ConvertOptions(column_types = {'ehail_fee': 'float64'}))
+# df = pv.read_csv("green_tripdata_2019-07.csv.gz", convert_options=pv.ConvertOptions(column_types = {'ehail_fee': 'float64'}))
 
-pr = pq.write_table(df, 'green_tripdata_2019-07.parquet')
+# pr = pq.write_table(df, 'green_tripdata_2019-07.parquet')
+src_file = "fhv_tripdata_2019-01.csv.gz"
+
+table = pv.read_csv(src_file, convert_options=pv.ConvertOptions(column_types = {'DOLocationID': 'float64', 'PUlocationID': 'float64' }))
+pq.write_table(table, src_file.replace('.csv.gz', '.parquet'))
